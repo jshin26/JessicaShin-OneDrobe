c871318b3ef2f6a57402e6b818e9f070b0d4e1cf
jhjshin <hyunjoo940926@gmail.com>
2020-06-12 12:50:42 -0400

    updated styling for <StyleLog> and its modal

--- client/src/Page/StyleLog/StyleLog.js
+++ client/src/Page/StyleLog/StyleLog.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactModal from 'react-modal';
+import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
+import ImageUploader from 'react-images-upload';
+
+import mask from '../../Asset/mask.jpg';
+import plus from '../../Asset/plus1.svg';
 
 import './StyleLog.scss';
 
 const API_URL="http://localhost:8080";
+
+function updateTime (unix) {
+    const getDate = new Date(unix);
+
+    const year = getDate.getFullYear();
+    const month = getDate.getMonth();
+    const day = getDate.getDate();
+
+
+    let calSeconds = Math.floor((new Date() - getDate) / 1000);
+    let timeAgo = Math.floor(calSeconds / 2592000);
+    if (timeAgo > 1) {
+    return (month+1) + '/' + day + '/' + year;
+    }
+    timeAgo = Math.floor(calSeconds / 86400);
+    if (timeAgo > 1) {
+      return timeAgo + " days ago";
+    }
+    timeAgo = Math.floor(calSeconds / 3600);
+    if (timeAgo > 1) {
+      return timeAgo + " hours ago";
+    }
+    timeAgo = Math.floor(calSeconds / 60);
+    if (timeAgo > 1) {
+      return timeAgo + " minutes ago";
+    }
+    return Math.floor(calSeconds) + " seconds ago";
+
+};
 
 class LogComp extends React.Component {
     state = {
@@ -27,10 +61,9 @@ class LogComp extends React.Component {
 
                 <div className="log__detail">
                     <p className="log__author">author: {this.props.author}</p>
-                    <p className="log__date">date: {this.props.date}</p>                    
-                </div>
-
-                <button className="log__read-btn" onClick={this.showMore}>{!this.state.show ? "open to read": "close"}</button>
+                    <p className="log__date">date: {updateTime(this.props.date)}</p>
+                    <button className="log__read-btn" onClick={this.showMore}>{!this.state.show ? "open to read": "close"}</button>
+                </div>                
 
                 <div className="log__read">
                     <p className="modal__read-desc">
@@ -46,7 +79,8 @@ class LogComp extends React.Component {
 class StyleLog extends React.Component {
     state={
         logs: [],
-        showModal: false
+        showModal: false,
+        pictures: []
     }
 
     componentDidMount () {
@@ -73,6 +107,37 @@ class StyleLog extends React.Component {
         })
     }
 
+    onDrop = (picture) => {
+        this.setState({
+            pictures: this.state.pictures.concat(picture)
+        })
+    }
+
+    postNewlog = (e) =>{
+        // e.preventDefault();
+        if (!e.target.author.value || !e.target.title.value || !e.target.description.value) {
+            alert ('please fill in the blanks');
+            e.preventDefault();
+        } else {
+            axios
+                .post(`${API_URL}/log/log`, {
+                    "id": uuidv4(),
+                    "author": e.target.author.value,
+                    "title" : e.target.title.value,
+                    "description" : e.target.description.value,
+                    "date" : Date.now(),
+                    "image" : mask
+                })
+                .then(res=> {
+                    console.log(res)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+                e.target.reset();
+        }
+    }
+
     render () {
         return (
             <main className="stylelog">
@@ -88,24 +153,49 @@ class StyleLog extends React.Component {
                             author={content.author}
                             description={content.description}
                         />
-                    })}
+                    }).reverse()}
 
-                    <button onClick={this.handleOpenModal} >
-                        <p>add post</p>
+                    <button className="stylelog__btn" onClick={this.handleOpenModal} >
+                        <img className="stylelog__icon" src={plus} alt="post"/>
                     </button>
 
                     <ReactModal
                         isOpen={this.state.showModal}
                         contentLabel="onRequestClose Example"
                         onRequestClose={this.handleCloseModal}
-                        // className="modal"
-                        // overlayClassName="overlay"
+                        className="log-modal"
+                        overlayClassName="log-overlay"
                     >
-                        <h2></h2>
-                        <form>
-                            <input type="text" name=""></input>
-                            
-                        </form>
+                        <div className="log-modal--inner">
+                            <h2 className="log-modal__title">join #stylelog</h2>
+                            <form className="log-modal__form" onSubmit={this.postNewlog}>
+                                <div className="log-modal__form-box">
+                                    <label className="log-modal__label">username</label>
+                                    <input className="log-modal__input" type="text" name="author" placeholder="please type username"></input>
+                                </div>
+                                <div className="log-modal__form-box">
+                                    <label className="log-modal__label">title</label>
+                                    <input className="log-modal__input" type="text" name="title" placeholder="please type title"></input>
+                                </div>
+                                <div className="log-modal__form-box">
+                                    <label className="log-modal__label">description</label>
+                                    <textarea className="log-modal__textarea" name="description" placeholder="please type description"></textarea>
+                                </div>
+    
+                                <ImageUploader 
+                                    withIcon={true}
+                                    buttonText='Choose images'
+                                    onChange={this.onDrop}
+                                    imgExtension={['.jpg', '.png', 'gif']}
+                                    maxFileSize={5242880}                                
+                                    className="log-modal__upload"
+                                />
+                                <div className="log-modal__btnbox">
+                                    <button className="log-modal__btn btn btn--white" onClick={this.handleCloseModal}>cancel</button>
+                                    <button className="log-modal__btn btn" type="submit">submit</button>
+                                </div>
+                            </form>
+                        </div>
 
                     </ReactModal>
 
